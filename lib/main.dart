3535056d6e2a80e849a7280e65a0139758f45f1a
Yader Campbell <yadermejiac123@gmail.com>
2024-06-17 22:24:23 -0600

    feat: We create our first read-only provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod_app/config/config.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const MyApp());
+import './config/config.dart';
+
+void main() => runApp(
+  const ProviderScope(child: MyApp())
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
