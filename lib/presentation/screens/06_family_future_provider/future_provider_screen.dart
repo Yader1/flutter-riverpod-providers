be1b93e0a03ea5526da23240b32ee06c5c69eb37
Yader Campbell <yadermejiac123@gmail.com>
2024-07-01 21:22:09 -0600

    feat: Future provider con argumentos

--- lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/screens/providers/providers.dart';
 
 
-class FamilyFutureScreen extends StatelessWidget {
+class FamilyFutureScreen extends ConsumerStatefulWidget {
   const FamilyFutureScreen({super.key});
 
   @override
+  FamilyFutureScreenState createState() => FamilyFutureScreenState();
+}
+
+class FamilyFutureScreenState extends ConsumerState<FamilyFutureScreen> {
+  int pokemonId = 3;
+
+  @override
   Widget build(BuildContext context) {
+    final pokemonAsync = ref.watch(pokemonProvider(pokemonId));
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Family Future Provider'),
+        title: Text('Pokemon id: $pokemonId'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (data) => Text(data), 
+          error: (error, stackTrace) => Text("Error $error"), 
+          loading: () => const CircularProgressIndicator()
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'btn-1',
+            child: const Icon( Icons.exposure_minus_1_outlined ),
+            onPressed: () {
+              if (pokemonId <= 1) return;
+              pokemonId --;
+              setState(() {});
+            },
+          ),
+          const SizedBox(height: 10.0),
+          FloatingActionButton(
+            heroTag: 'btn-2',
+            child: const Icon( Icons.exposure_plus_1 ),
+            onPressed: () {
+              pokemonId++;
+              setState(() {});
+            },
+          ),
+        ],
       ),
     );
   }
