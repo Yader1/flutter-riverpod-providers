9aa62f985a002fc76a7d87273ef92401f127b1d7
Yader Campbell <yadermejiac123@gmail.com>
2024-07-01 23:45:47 -0600

    feat: Stream provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/screens/providers/providers.dart';
 
 
 class StreamProviderScreen extends StatelessWidget {
@@ -10,6 +12,34 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersInChatAsync = ref.watch(usersInChatProvider);
+
+    if(usersInChatAsync.isLoading){
+      return const CircularProgressIndicator();
+    }
+    if(usersInChatAsync.hasError){
+      return Center(
+        child: Text('${usersInChatAsync.error}'),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: usersInChatAsync.value!.length,
+      itemBuilder: (BuildContext context, int index){
+        return ListTile(
+          title: Text(usersInChatAsync.value![index]),
+        );
+      }
     );
   }
 }
